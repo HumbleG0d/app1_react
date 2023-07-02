f38142a4cfa84166503cb3125ccaf6debf5a9662
HumbleG0d <sergiouni2000@gmail.com>
2023-07-02 07:06:02 -0500

    change static data to dynamic data

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import imgEmma from '../images/testimonio-emma.png'
 import '../stylesheets/Testimonio.css'
 
-export function Testimonio(){
+export function Testimonio(props){
 	return(
 		<div className='contenedor-testimononio'>
 			<img className='imagen-testimonio'
-				src={imgEmma}
-				alt = 'Foto de Emma'/>
+				src={props.img}
+				alt = {`Foto de ${props.name}`}/>
 			<div className='texto__contenedor-testimonio'>
-				<p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-				<p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-				<p className='texto-testimonio'>
-        &#34;I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.&#34;
-				</p>
+				<p className='nombre-testimonio'><strong>{props.name}</strong> in {props.country}</p>
+				<p className='cargo-testimonio'>{props.cargo} at <strong>{props.company}</strong></p>
+				<p className='texto-testimonio'>&#34;{props.testimony}&#34;</p>
 			</div>
 		</div>
 	)  
