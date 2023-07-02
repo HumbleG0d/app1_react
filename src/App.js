103a250043d1259c697fdc507a90203a327c6e42
HumbleG0d <sergiouni2000@gmail.com>
2023-07-02 07:23:40 -0500

    change text testimony

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ function App() {
 					country = 'Singapore'
 					cargo = 'Software Engineer'
 					company = 'Amazon'
-					testimony = 'It s scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.'/>
+					testimony = {
+						<> It&#39;s scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life</strong>.</>
+					} />
           
 
 				<Testimonio 
@@ -25,7 +27,9 @@ function App() {
 					country = 'Nigeria'
 					cargo = 'Software Engineer'
 					company = 'ChatDesk'
-					testimony = 'freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'/>
+					testimony = {
+						<> <strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</>
+					}/>
           
 				<Testimonio 
 					name = 'Emma Bostian'
@@ -33,7 +37,9 @@ function App() {
 					country = 'Suecia'
 					cargo = 'Software Engineer'
 					company = 'Spotify'
-					testimony = 'I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify'/>
+					testimony = {
+						<>I&#39;ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamp&#39;s course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify</>
+					}/>
 
 			</div>
 		</div>
