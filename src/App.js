c5642a00ec60f1913f7889b287822b798f514a50
HumbleG0d <sergiouni2000@gmail.com>
2023-07-03 17:20:25 -0500

    change the way our component is used for this I used the map function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import './normalize.css'
 import './App.css'
 import { Testimonio } from './components/Testimonio'
-import imgEmma from './images/testimonio-emma.png'
-import imgSarah from './images/testimonio-sarah.png'
-import imgShawn from './images/testimonio-shawn.png'
+import { PEOPLE } from './people/people'
+
 
 function App() {
 	return (
 		<div className="App">
 			<div className='contenedor-principal'>
 				<h1 className='title_contenedor'>Esto es lo que dicen nuestros alumnos de freeCodeCamp:</h1>
-				<Testimonio 
-					name = 'Shawn Wang'
-					img = {imgShawn}
-					country = 'Singapore'
-					cargo = 'Software Engineer'
-					company = 'Amazon'
-					testimony = {
-						<> It&#39;s scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life</strong>.</>
-					} />
-          
-
-				<Testimonio 
-					name = 'Sarah Chima'
-					img = {imgSarah}
-					country = 'Nigeria'
-					cargo = 'Software Engineer'
-					company = 'ChatDesk'
-					testimony = {
-						<> <strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</>
-					}/>
-          
-				<Testimonio 
-					name = 'Emma Bostian'
-					img = {imgEmma}
-					country = 'Suecia'
-					cargo = 'Software Engineer'
-					company = 'Spotify'
-					testimony = {
-						<>I&#39;ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamp&#39;s course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify</>
-					}/>
-
+				{
+					PEOPLE.map((person , index) => (
+						<Testimonio
+							key = {index} 
+							name = {person.name}
+							country = {person.country}
+							company = {person.company}
+							cargo = {person.cargo}
+							img = {person.img}
+							testimony = {person.testimony}
+						/>
+					))
+				}        
 			</div>
 		</div>
 	)
